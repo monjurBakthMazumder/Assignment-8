6488b894a56e5f99c3548ddaba6b14501d38065f
md Monjur Bakth Mazumder <md.monjurmbm2001@gmail.com>
2023-09-24 13:44:48 -0700

    show all donation data from local storage on donation path

---
+++ src/Component/DonationCard/DonationCard.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const DonationCard = ({donation}) => {
+    const {id, title, category, image2, color, price} =  donation || {}
+    console.log(donation);
+    return (
+        <div className="card shadow-xl rounded flex flex-row items-center" style={{backgroundColor: color?.card_bg}}>
+            <figure className='rounded-r-none'><img src={image2} alt={`images of ${title}`} className='w-full'/></figure>
+            <div className="card-body p-3 w-1/2">
+                <span className='w-fit px-2 py-[2px] text-sm font-medium rounded' style={{backgroundColor: color?.category_bg, color: color?.text_color}}>{category}</span>
+                <h2 className="text-lg lg:text-xl font-bold">{title}</h2>
+                <span className='font-bold'  style={{color: color?.text_color}}>${price.toFixed(2)}</span>
+                <Link to={`/${id}`} className='btn w-fit border-0 text-white btn-sm rounded' style={{backgroundColor: color?.text_color}}>View Details</Link>
+            </div>
+        </div>
+    );
+};
+
+DonationCard.propTypes = {
+    donation : PropTypes.object,
+};
+
+export default DonationCard;
--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -1,8 +1,49 @@
+import { useEffect, useState } from "react";
+import useGetAllData from "../../Hock/useGetAllData";
+import { getSaveDonation } from "../../Utility/localStogare";
+import DonationCard from "../../Component/DonationCard/DonationCard";
 
 const Donation = () => {
+    const [donations, setDonations] = useState()
+    const [dataLength, setDataLength] = useState(4)
+    const [allData] = useGetAllData()
+
+    useEffect(()=>{
+        const saveDonationId = getSaveDonation()
+        console.log(saveDonationId);
+
+        if(allData?.length > 0){
+            const donationsArray =[];
+            for(const id of saveDonationId){
+                const donations = allData?.find(data=> data?.id === id)
+                if(donations){
+                    donationsArray.push(donations)
+                }
+                setDonations(donationsArray)
+        }
+        }
+    },[allData])
     return (
-        <div>
-            Donation
+        <div className="px-5 md:px-[10%] mt-10 mb-40">
+            {
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+                    {
+                        donations?.slice(0,dataLength).map(donation=> 
+                            <DonationCard
+                                key={donation.id}
+                                donation={donation}
+                            />
+                        )
+                    }
+                </div>
+                
+            }
+            <div className={`text-center my-5 ${dataLength === donations?.length && 'hidden'}`}>
+                <button 
+                    onClick={()=> setDataLength(donations.length)}
+                    className="btn btn-success bg-[#009444] text-white"
+                >See All</button>
+            </div>
         </div>
     );
 };
